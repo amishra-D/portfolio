dc380da3d92ed55ce8a7d5cfab087e9054162a33
Anshu Mishra <anshumishraocog@gmail.com>
2025-03-29 23:48:27 +0530

    correct path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true, // Ensures changes are picked up
-    },
-    hmr: false, // ❌ Disables WebSocket HMR in production
-  }
+  plugins: [react()],
+  base: '/portfolio/', // Change this to your actual repository name
 });
