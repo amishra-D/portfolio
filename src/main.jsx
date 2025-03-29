7ba527e4b39147cb9c92a7a99fa5d811265e1f2e
Anshu Mishra <anshumishraocog@gmail.com>
2025-03-29 22:50:30 +0530

    with router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { HashRouter as Router } from "react-router-dom";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+    <Router>
     <App />
+    </Router>
 )
