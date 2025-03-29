d01eb5b568a3a2f90486c60af993405f5218d335
Anshu Mishra <anshumishraocog@gmail.com>
2025-03-29 23:27:42 +0530

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true, // Ensures changes are picked up
+    },
+    hmr: false, // ❌ Disables WebSocket HMR in production
+  }
+});
